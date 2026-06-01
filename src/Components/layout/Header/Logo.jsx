d16e3019a3dc6642025de28a0120a18f839ee2c1
Jatin-jay-31 <jatinjaisharma916@gmail.com>
2026-06-01 10:04:44 +0530

    Update Logo.jsx

--- src/Components/layout/Header/Logo.jsx
+++ src/Components/layout/Header/Logo.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import notesIcon from "../../../assets/3d-notes.png"
 
 function Logo() {
   const navigate=useNavigate()
@@ -10,7 +11,7 @@ function Logo() {
         }
   return (
     <div className='w-10 mx-2'>
-      <span> <img src="src/assets/3d-notes.png" alt="img" onClick={()=> navigate(navItem.path)} /> </span>
+      <span> <img src={notesIcon} alt="img" onClick={()=> navigate(navItem.path)} /> </span>
     </div>
   )
 }
